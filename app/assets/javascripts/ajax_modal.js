d82bea6840173307e34e0c7135abe7bf4f5ecf98
Phil Plencner <philip_plencner@harvard.edu>
2019-05-10 11:18:47 -0400

    Fixed search results icons and repository modal link.

--- app/assets/javascripts/ajax_modal.js
+++
@@ -1,8 +0,0 @@
-/* Override some functionality in the Blacklight ajax_modal.js to allow display of the modal dialog
-   using the Foundation framework rather than Bootstrap
- */
-
-/* Show the modal using the Foundation library, after the content has been loaded */
-$(document).on('loaded.blacklight.blacklight-modal', function() {
-    jQuery('#ajax-modal').foundation('reveal', 'open');
-});
