b79f646b87ed340918833b1ed517389d7dfdd54b
Cédric Viaccoz <cedric.viaccoz@epfl.ch>
2019-07-09 14:50:49 -0400

    HLDP-331 - signin/signout redirect
    Redirect to search results page on sign-in and sign-out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,14 @@ class ApplicationController < ActionController::Base
   layout 'blacklight'
 
   protect_from_forgery with: :exception
+
+  protected
+
+  def after_sign_in_path_for(resource)
+    "/catalog?search_field=all_fields&q="
+  end
+
+  def after_sign_out_path_for(resource)
+    "/catalog?search_field=all_fields&q="
+  end
 end
