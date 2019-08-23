1b458fe00814dca6f87792be42111deb1cee718a
Cédric Viaccoz <cedric.viaccoz@epfl.ch>
2019-08-23 15:51:54 -0400

    Changed occurences of 'full text' to 'raw text'

--- config/routes.rb
+++ config/routes.rb
@@ -36,7 +36,7 @@ Rails.application.routes.draw do
     end
   end
 
-  get '/fulltext/:id', to: 'fulltexts#index'
+  get '/rawtext/:id', to: 'fulltexts#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
