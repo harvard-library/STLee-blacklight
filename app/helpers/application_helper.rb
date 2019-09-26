7e322a69cbf8d44ce148c655b9922f9e0f0de18c
vac765 <vac765@172.31.98.32>
2019-09-25 20:52:04 -0400

    Bugfix for Download This Item button after IIIF Manifest update.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -40,33 +40,27 @@ module ApplicationHelper
   end
 
   def generate_twitter_meta_tags
-    twitter_meta_tags = '<!-- BEGIN TWITTER SUMMARY CARD -->
-                          <meta name="twitter:card" content="summary_large_image">
-                          <meta name="twitter:title" content="Harvard Digital Collections">
-                          <meta name="twitter:site" content="@harvardlibrary">
-                          <meta name="twitter:url" content="https://digitalcollections.library.harvard.edu">
-                          <meta name="twitter:image" content="%s">'
-    base_img_url = 'https://library.harvard.edu/sites/default/files/home-background-eclipse.jpg'
-    if defined?(@document) and @document[:drs_file_id] 
-      ids_download_url = 'https://ids.lib.harvard.edu/ids/iiif/%d/full/1200,/0/default.jpg'
-      if @document[:delivery_service] == 'ids'
-        base_img_url = ids_download_url % @document[:drs_file_id]
-      elsif @document[:delivery_service] == 'pds'
-        #in the case of multiple image content, we need to get the ids id of the cover image using the iiif manifest data.
-        manifest_url = 'https://iiif.lib.harvard.edu/manifests/drs:'+@document[:drs_file_id]
-        res = retrieve_still_image_json_metadata manifest_url
-        if res
-          #the cover image manifest url will always be the first element from the array 'canvases'
-          cover_img_manifest_url = res['structures'][0]['canvases'][0]
-          if cover_img_manifest_url
-            matches = cover_img_manifest_url.match('canvas-(.*).json')
-            if matches[1]
-              base_img_url = ids_download_url % matches[1]
-            end
+      twitter_meta_tags = '<!-- BEGIN TWITTER SUMMARY CARD -->
+                            <meta name="twitter:card" content="summary_large_image">
+                            <meta name="twitter:title" content="Harvard Digital Collections">
+                            <meta name="twitter:site" content="@harvardlibrary">
+                            <meta name="twitter:url" content="https://digitalcollections.library.harvard.edu">
+                            <meta name="twitter:image" content="%s">'
+      base_img_url = 'https://library.harvard.edu/sites/default/files/home-background-eclipse.jpg'
+      if defined?(@document) and @document[:drs_file_id]
+        ids_download_url = 'https://ids.lib.harvard.edu/ids/iiif/%d/full/1200,/0/default.jpg'
+        if @document[:delivery_service] == 'ids'
+          base_img_url = ids_download_url % @document[:drs_file_id]
+        elsif @document[:delivery_service] == 'pds'
+          #in the case of multiple image content, we need to get the ids id of the cover image using the iiif manifest data.
+          manifest_url = 'https://iiif.lib.harvard.edu/manifests/drs:'+@document[:drs_file_id]
+          res = retrieve_still_image_json_metadata manifest_url
+          if res
+            #the cover image manifest url will always be the first element from the array 'canvases'
+            base_img_url = res['sequences'][0]['canvases'][0]['thumbnail']['@id']
           end
         end
       end
+      (twitter_meta_tags % base_img_url).html_safe
     end
-    (twitter_meta_tags % base_img_url).html_safe
-  end
 end
